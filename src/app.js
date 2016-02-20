f227a20172b3d43a1f755cffb0d009bdac8f4c33
Doolan <doolansr@rose-hulman.edu>
2016-02-20 11:42:41 -0500

    routing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
-(function() {
+(function () {
     var app = angular.module('TimeCapsule', ['ngRoute']);
 
-    //$('#simple-menu').sidr();
-
-    //$('#fullpage').fullpage();
+    app.config(["$routeProvider", function ($routeProvider, $routeParams) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: '../views/home.html'
+                // no controller needed for home
+            })
+            .when('/dashboard', {
+                templateUrl: '../views/dashboard.html'
+            })
+            .otherwise({
+                redirectTo: '/home'
+            });
+    }]);
+    app.exports = app;
 })();
--- src/js/controller.js
+++ src/js/controller.js
@@ -2,7 +2,7 @@
  * Created by Steven on 2/20/2016.
  */
 angular.module('TimeCapsule')
-    .controller('landingController', function ($scope){
+    .controller('landingController', function ($scope, $location){
         $scope.scrollDown = function() {
             $('html, body').animate({
                 scrollTop: $("#about").offset().top
@@ -12,13 +12,17 @@ angular.module('TimeCapsule')
         $scope.launchLogin = function(){
             $('#login-modal')
                 .modal('show').modal('refresh')//re-centers
-                .modal('attach events', '#login-modal .remove', 'hide')
+                //.modal('attach events', '#login-modal .remove', 'hide')
                 .modal({
                     closable: false,
                     onDeny:function () {
                         return true;
                     },
-                    onApprove: $scope.loginConfirm
+                    onApprove: function(){
+                        //$scope.loginConfirm;
+                        return true;
+                    }
+
                 });
             $('#login-modal .form .message').hide();
 
@@ -61,7 +65,26 @@ angular.module('TimeCapsule')
                 $('#login-modal .form .message').show();
                 return false;
             }
+            console.log("I was hit");
+            $location.path('/dashboard/');
+            //$scope.redirectFunction();
+            //$location.redirect('/dashboard/');
             //Redirect
             return true;
         };
+
+    })
+    .controller('loginController', function($scope, $location){
+        $scope.redirectFunction = function(){
+            if (!$('#login-modal .ui.form').form('validate form')) {
+                $('#login-modal .form .message').show();
+                return false;
+            }else {
+                $('#login-modal')
+                    .modal('hide');
+                console.log("I was hit");
+                $location.path('/dashboard/');
+                return true;
+            }
+        }
     });
